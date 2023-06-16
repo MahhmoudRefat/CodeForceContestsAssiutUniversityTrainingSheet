d57e3a179689a2e7b98477b6b1e42846f7ac2314
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-16 10:07:25 +0300

    new class for the accepted contests

---
+++ src/main/kotlin/AcceptedContests.kt
@@ -0,0 +1,10 @@
+class AcceptedContests {
+/*
+* Given a name S. Print "Hello, (name)" without parentheses.
+* The code
+
+* var name = readln().toString()
+        println("Hello, $name")
+* */
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
     fun main(args: Array<String>) {
-        var name = readln().toString()
-        println("Hello, $name")
+
     }
 }
