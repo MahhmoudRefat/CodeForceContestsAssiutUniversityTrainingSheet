85c112a7835f47053622bd2152769d61da394ab7
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-16 09:58:43 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    fun main(args: Array<String>) {
+        var name = readln().toString()
+        println("Hello, $name")
+    }
+}
