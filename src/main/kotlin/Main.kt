3b6131907d0634a14a0787fa9bdbfe7be977f005
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-17 18:47:27 +0300

    Basic Data Types contest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-
+    var intvariable: Int = readln().toInt()
+    var longvariable: Long = readln().toLong()
+    var charvariable: Char = readln().single()
+    var floatvariable: Float = readln().toFloat()
+    var doubvariable: Double = readln().toDouble()
+    println(intvariable)
+    println(longvariable)
+    println(charvariable)
+    println(floatvariable)
+    println(doubvariable)
 }
