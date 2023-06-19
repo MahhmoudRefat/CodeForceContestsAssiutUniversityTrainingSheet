17e134cb208278651897945d43484bec2409f917
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-19 21:49:20 +0300

    difference Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 fun main() {
-
-
+    val inputLine = readLine()!!.trim()
+    var (aa, bb, cc, dd) = inputLine.split(" ", limit = 4)
+    var a = aa.toInt()
+    var b = bb.toLong()
+    var c = cc.toInt()
+    var d = dd.toLong()
+    var x = (a * b) - (c * d)
+    println("Difference = $x")
 }
