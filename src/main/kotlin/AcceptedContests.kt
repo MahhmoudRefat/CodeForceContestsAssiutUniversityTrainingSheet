83c71ce470fac0ddaa758cba8c3d9ac7fc62d0b2
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-23 12:48:30 +0300

    h- Two numbers Problem in class  code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -80,4 +80,21 @@ class AcceptedContests {
     }/////////
     println((x*(x+1))/2)
      */
+
+    /*
+    h- Two numbers
+       var inputline = readLine()!!.trim()
+    var (xx, yy) = inputline.split(" ", limit = 2)
+    var x = xx.toFloat()
+    var y = yy.toFloat()
+    val floorResult = floor(x/y)
+    val ceilResult = ceil(x/y)
+    val roundResult = (x/y).roundToInt()
+    println("floor ${x.toInt()} / ${y.toInt()} = ${floorResult.toInt()}")
+    println("ceil ${x.toInt()} / ${y.toInt()} = ${ceilResult.toInt()}")
+    println("round ${x.toInt()} / ${y.toInt()} = ${roundResult.toInt()}")
+
+    //var floor : Int = x / y
+    //  var ceil : Int = floor + 1
+     */
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,5 @@ import kotlin.math.floor
 import kotlin.math.roundToInt
 
 fun main() {
-    var inputline = readLine()!!.trim()
-    var (xx, yy) = inputline.split(" ", limit = 2)
-    var x = xx.toFloat()
-    var y = yy.toFloat()
-    val floorResult = floor(x/y)
-    val ceilResult = ceil(x/y)
-    val roundResult = (x/y).roundToInt()
-    println("floor ${x.toInt()} / ${y.toInt()} = ${floorResult.toInt()}")
-    println("ceil ${x.toInt()} / ${y.toInt()} = ${ceilResult.toInt()}")
-    println("round ${x.toInt()} / ${y.toInt()} = ${roundResult.toInt()}")
 
-    //var floor : Int = x / y
-    //  var ceil : Int = floor + 1
 }
