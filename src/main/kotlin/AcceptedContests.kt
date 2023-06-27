485e158fc56c4ed9651176f4859db5648efb9177
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-06-27 11:54:15 +0300

    K. Max and Min  Problem code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -114,4 +114,18 @@ import kotlin.math.roundToInt
         println("No")
     }
      */
+    /*
+    J. Multiples
+
+    var inputreader = readln().trim()
+    var (aa,bb) = inputreader.split(" ")
+    var a = aa.toInt()
+    var b = bb.toInt()
+    if (a%b == 0 || b%a == 0 ){
+        println("Multiples")
+    }
+    else {
+        println("No Multiples")
+    }
+     */
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,26 @@
 fun main() {
 var inputreader = readln().trim()
-    var (aa,bb) = inputreader.split(" ")
+    var (aa,bb,cc) = inputreader.split(" ")
     var a = aa.toInt()
     var b = bb.toInt()
-    if (a%b == 0 || b%a == 0 ){
-        println("Multiples")
-    }
-    else {
-        println("No Multiples")
+    var c = cc.toInt()
+    var maxtemp = 0
+    var mintemp = 0
+    if (a >= b && a >= c ){
+        maxtemp = a
+    }else if (b >= a && b >= c ){
+        maxtemp = b
+    }else if (c >= a && c >= b ){
+        maxtemp = c
     }
 
+    if (a <= b && a <= c ){
+        mintemp = a
+    }else if (b <= a && b <= c ){
+        mintemp = b
+    }else if (c <= a && c <= b ){
+        mintemp = c
+    }
+
+println("$mintemp $maxtemp")
 }
