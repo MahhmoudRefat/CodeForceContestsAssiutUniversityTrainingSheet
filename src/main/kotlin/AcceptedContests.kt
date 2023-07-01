629d37794fbf38c000f143cdb818c6d0520189c6
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-01 15:59:21 +0300

    L. The Brothers Problem code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -128,4 +128,32 @@ import kotlin.math.roundToInt
         println("No Multiples")
     }
      */
+    /*
+    K. Max and Min
+
+    var inputreader = readln().trim()
+    var (aa,bb,cc) = inputreader.split(" ")
+    var a = aa.toInt()
+    var b = bb.toInt()
+    var c = cc.toInt()
+    var maxtemp = 0
+    var mintemp = 0
+    if (a >= b && a >= c ){
+        maxtemp = a
+    }else if (b >= a && b >= c ){
+        maxtemp = b
+    }else if (c >= a && c >= b ){
+        maxtemp = c
+    }
+
+    if (a <= b && a <= c ){
+        mintemp = a
+    }else if (b <= a && b <= c ){
+        mintemp = b
+    }else if (c <= a && c <= b ){
+        mintemp = c
+    }
+
+println("$mintemp $maxtemp")
+     */
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,11 @@
 fun main() {
-var inputreader = readln().trim()
-    var (aa,bb,cc) = inputreader.split(" ")
-    var a = aa.toInt()
-    var b = bb.toInt()
-    var c = cc.toInt()
-    var maxtemp = 0
-    var mintemp = 0
-    if (a >= b && a >= c ){
-        maxtemp = a
-    }else if (b >= a && b >= c ){
-        maxtemp = b
-    }else if (c >= a && c >= b ){
-        maxtemp = c
+    var inputline = readln().trim()
+    var (f1, s1) = inputline.split(" ")
+    var inputline2 = readln().trim()
+    var (f2, s2) = inputline2.split(" ")
+    if (s1 == s2) {
+        println("ARE Brothers")
+    } else {
+        println("NOT")
     }
-
-    if (a <= b && a <= c ){
-        mintemp = a
-    }else if (b <= a && b <= c ){
-        mintemp = b
-    }else if (c <= a && c <= b ){
-        mintemp = c
-    }
-
-println("$mintemp $maxtemp")
 }
