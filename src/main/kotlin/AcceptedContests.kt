e1d5e9b49b57b31c8b3fd6b2bfda1b56145ee3e9
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-03 18:09:34 +0300

    M. Capital or Small or Digit Problem code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -171,6 +171,21 @@ println("$mintemp $maxtemp")
         println("NOT")
     }
      */
+/*
+M. Capital or Small or Digit
 
+ var xx = readLine()!![0]
+    var x = xx.code
+    if (x in 48..64) {
+        println("IS DIGIT")
+    } else if (x in 65..96) {
+        println("ALPHA")
+        println("IS CAPITAL")
+    } else {
+        println("ALPHA")
+        println("IS SMALL")
+
+}
+ */
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,4 @@
 fun main() {
-    var xx = readLine()!![0]
-    var x = xx.code
-    if (x in 48..64) {
-        println("IS DIGIT")
-    } else if (x in 65..96) {
-        println("ALPHA")
-        println("IS CAPITAL")
-    } else {
-        println("ALPHA")
-        println("IS SMALL")
-    }
+    
+
 }
