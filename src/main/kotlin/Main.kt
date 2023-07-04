409c5e5ecb35d7c064b1729843294e2098ac89eb
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-04 15:55:53 +0300

    N - Char Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 fun main() {
-    
-
+    var inputeline = readln()[0]
+    var x = inputeline.code
+    var x2 : Int = 0
+    if (x in 65..96) {
+        x2 = x + 32
+    } else {
+        x2 = x - 32
+    }
+    println(x2.toChar())
 }
