50730989960a49673450e9a53f0067b6cf214b6d
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-05 12:41:12 +0300

    P - First digit !  Problem code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -187,5 +187,17 @@ M. Capital or Small or Digit
 
 }
  */
+/*
+N - Char
 
+ var inputeline = readln()[0]
+    var x = inputeline.code
+    var x2 : Int = 0
+    if (x in 65..96) {
+        x2 = x + 32
+    } else {
+        x2 = x - 32
+    }
+    println(x2.toChar())
+ */
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,28 @@
 fun main() {
-    var inputeline = readln()[0]
-    var x = inputeline.code
-    var x2 : Int = 0
-    if (x in 65..96) {
-        x2 = x + 32
+    var readinput = readln().trim()
+    var x: Int = readinput.first().code
+
+    if (x % 2 == 0) {
+        println("EVEN")
     } else {
-        x2 = x - 32
+        println("ODD")
     }
-    println(x2.toChar())
 }
+/*
+var readinput = readLine()!!.trim()
+    var (aa, ss, bb) = readinput.split("")
+    var a = aa.toInt()
+    var b = bb.toInt()
+    var s = ss[0]
+    var result : Int= 0
+    if (s == '+') {
+        result = a + b
+    } else if (s == '−') {
+        result = a - b
+    } else if (s == '*') {
+        result = a * b
+    } else {
+        result = a / b
+    }
+    println(result)
+ */
