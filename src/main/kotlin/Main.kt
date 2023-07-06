5ef3f1528866d87c420a30763b5efcb4045d479e
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-06 23:05:44 +0300

    Q. Coordinates of a Point  Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,23 @@
 fun main() {
+    var readinput = readLine()!!.trim()
+    var (xx, yy) = readinput.split(" ")
+    var x = xx.toFloat()
+    var y = yy.toFloat()
+    if (x == 0f && y == 0f) {
+        println("Origem")
+    } else if (x != 0f && y == 0f) {
+        println("Eixo X")
+    } else if (x == 0f && y != 0f) {
+        println("Eixo Y")
+    } else if (x > 0 && y > 0) {
+        println("Q1")
+    } else if (x < 0 && y > 0) {
+        println("Q2")
+    } else if (x > 0 && y < 0) {
+        println("Q4")
+    } else if (x < 0 && y < 0) {
+        println("Q3")
+    }
 
 }
 /*
