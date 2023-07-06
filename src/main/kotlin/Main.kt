497666d19e4658bfcad2602fc190ccc6c25b7c48
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-07 00:25:56 +0300

    R - Age in Days Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 fun main() {
-
+    var readinput = readLine()!!.trim()
+    var n = readinput.toLong()
+    var years = n / 365
+    n %= 365
+    var monthes = (n / 30 )
+    n %= 30
+    var days = n
+    println("$years years")
+    println("$monthes months")
+    println("$days days")
 
 }
 /*
