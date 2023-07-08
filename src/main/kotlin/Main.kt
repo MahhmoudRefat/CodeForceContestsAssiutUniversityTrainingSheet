8c999d235aa68f722ab6e38e72c1fb631007ce8b
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-08 19:14:40 +0300

    S - Interval  Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,7 @@
 fun main() {
     var readinput = readLine()!!.trim()
     var x = readinput.toFloat()
-    if (x in 0f .. 25f  ){
-        println("Interval [0,25]")
-    }
-    else if(x in 25f .. 50f  ){
-        println("Interval (25,50]")
-    }
-    else if(x in 50f .. 75f  ){
-        println("Interval (50,75]")
-    }
-    else if(x in 75f .. 100f  ){
-        println("Interval (75,100]")
-    }
-    else {
-        println("Out of Intervals")
-    }
+
 
 }
 /*
