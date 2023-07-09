1b535304aaa3986aaf630ab4d6fc5663f3882d25
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-09 16:29:37 +0300

    T. Sort Numbers  Problem code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -270,4 +270,48 @@ var readinput = readLine()!!.trim()
         println("Out of Intervals")
     }
      */
+    /*
+    T. Sort Numbers
+
+     var readinput = readLine()!!.trim()
+    var (aa, bb, cc) = readinput.split(" ")
+    var a = aa.toInt()
+    var b = bb.toInt()
+    var c = cc.toInt()
+    if (a <= b && a <= c) {
+        if (b <= c) {
+            println("$a")
+            println("$b")
+            println("$c")
+        } else {
+            println("$a")
+            println("$c")
+            println("$b")
+        }
+    } else if (b <= a && b <= c) {
+        if (a <= c) {
+            println("$b")
+            println("$a")
+            println("$c")
+        } else {
+            println("$b")
+            println("$c")
+            println("$a")
+        }
+    } else if (c <= a && c <= b) {
+        if (a <= b) {
+            println("$c")
+            println("$a")
+            println("$b")
+        } else {
+            println("$c")
+            println("$b")
+            println("$a")
+        }
+    }
+    println("")
+    println("$a")
+    println("$b")
+    println("$c")
+     */
 }
