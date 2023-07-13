174f8265d9c71d9051bb2b521775761f55694cbf
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-13 07:55:08 +0300

    V - Comparison  Problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,17 @@
 fun main() {
     var readinput = readLine()!!.trim()
-    //var (aa, bb) = readinput.split(" ")
-    var a = readinput.toFloat()
-
-
-}
-/*
-var readinput = readLine()!!.trim()
-    var (aa, ss, bb) = readinput.split("")
+    var (aa, ss, bb) = readinput.split(" ")
     var a = aa.toInt()
+    var s = ss.toString()
     var b = bb.toInt()
-    var s = ss[0]
-    var result : Int= 0
-    if (s == '+') {
-        result = a + b
-    } else if (s == '−') {
-        result = a - b
-    } else if (s == '*') {
-        result = a * b
-    } else {
-        result = a / b
+    when (s) {
+        ">" -> if (a > b) println("Right")
+        else println("Wrong")
+
+        "<" -> if (a < b) println("Right")
+        else println("Wrong")
+
+        "=" -> if (a == b) println("Right")
+        else println("Wrong")
     }
-    println(result)
- */
+}
