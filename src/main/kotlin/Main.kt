e08f08a93edae629f3e0f7128dc303615cd7be2b
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-16 08:14:59 +0300

    Y - The last 2 digits  Problem in class code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,14 @@
-import kotlin.math.max
-import kotlin.math.min
-
 fun main() {
     var readinput = readLine()!!.trim()
-    var (ll1, rr1 , ll2,rr2) = readinput.split(" ")
-    var l1 = ll1.toInt() // the first number
-    var r1 = rr1.toInt() // the second number
-    var l2 = ll2.toInt() // the result
-    var r2 = rr2.toInt() // the result
-if (max(l1,l2) > min(r1,r2)) println("-1")
-    else {
-        println("${max(l1,l2)} ${min(r1,r2)}")
+    var (aa, bb, cc, dd) = readinput.split(" ")
+    var a = aa.toLong() % 100
+    var b = bb.toLong() % 100
+    var c = cc.toLong() % 100
+    var d = dd.toLong() % 100
+    var multiplication = (a * b * c * d) % 100
+    if (multiplication < 10) {
+        println("0$multiplication")
+    } else {
+        println(multiplication)
     }
 }
