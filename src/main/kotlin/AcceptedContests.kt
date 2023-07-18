461c3568e6a71d1afd81526adf9e4baa25ede81c
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-18 10:52:49 +0300

    A - 1 to N  Problem in class code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -401,4 +401,18 @@ if (max(l1,l2) > min(r1,r2)) println("-1")
         println(multiplication)
     }
      */
+/*
+ var readinput = readLine()!!.trim()
+    var (aa, bb, cc, dd) = readinput.split(" ")
+    var a = aa.toDouble()
+    var b = bb.toDouble()
+    var c = cc.toDouble()
+    var d = dd.toDouble()
+    if (b * Math.log(a) > d *Math.log(c)) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+ */
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
 fun main() {
-    var readinput = readLine()!!.trim()
+
+var readinput = readLine()!!.trim()
+    var (nn) = readinput.split(" ")
+    var n = nn.toInt()
+for (i in 1 .. n )
+{
+    println(i)
+}
+    /* var readinput = readLine()!!.trim()
     var (aa, bb, cc, dd) = readinput.split(" ")
     var a = aa.toDouble()
     var b = bb.toDouble()
     var c = cc.toDouble()
-    var d = dd.toDouble()
-    if (b * Math.log(a) > d *Math.log(c)) {
-        println("YES")
-    } else {
-        println("NO")
-    }
+    var d = dd.toDouble() */
+
 }
