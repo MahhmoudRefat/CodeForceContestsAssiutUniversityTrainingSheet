cd036957cdbb6c8eceb743ae8fa1c2cf1d8c3c31
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-19 15:19:32 +0300

    B. Even Numbers problem in code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
 fun main() {
 
-var readinput = readLine()!!.trim()
+    var readinput = readLine()!!.trim()
     var (nn) = readinput.split(" ")
     var n = nn.toInt()
-
+    if (n == 1 || n == 0 ) {
+        println("-1")
+    } else {
+        for (i in 1..n) {
+            if (i % 2 == 0) {
+                println(i)
+            }
+        }
+    }
+}
     /* var readinput = readLine()!!.trim()
     var (aa, bb, cc, dd) = readinput.split(" ")
     var a = aa.toDouble()
@@ -11,4 +20,4 @@ var readinput = readLine()!!.trim()
     var c = cc.toDouble()
     var d = dd.toDouble() */
 
-}
+
