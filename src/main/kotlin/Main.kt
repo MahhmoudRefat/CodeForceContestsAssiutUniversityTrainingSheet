18463e2e9f5ab8cc14139cece688ee61c427628d
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-07-25 19:03:03 +0300

    D - Fixed Password problem code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,78 @@
+import kotlin.system.exitProcess
+
 fun main() {
+    //var readinput = readLine()!!.trim()
+    //var (aa, bb, cc, dd ,ee) = readinput.split(" ")
+    while (true) {
+        var pass = readln().toInt()
+        if (pass == 1999) {
+            println("Correct")
+            exitProcess(0)
+        } else {
+            println("Wrong")
+        }
+    }
 
-    var readinput = readLine()!!.trim()
-    var (nn) = readinput.split(" ")
-    var n = nn.toInt()
 
+
+    /*
+
+
+
+        var n = readln().toInt()
+    //    var h = mutableListOf<Int>();
+        var counter_Even = 0
+        var counter_odd = 0
+        var counter_positive = 0
+        var counter_negative = 0
+        for (i in 1..n) {
+
+        }
+        for (i in 1..n) {
+            var num = readln().toInt()
+            if (num % 2 == 0) {
+                counter_Even++
+            }
+            else   {
+                counter_odd++
+            }
+
+            if (num > 0) {
+                counter_positive++
+            }
+            else if ((num < 0)) {
+                counter_negative++
+            }
+
+           // h.add(readinput2.toInt())
+        }
+        println("Even : $counter_Even")
+        println("Odd : $counter_odd")
+        println("Positive : $counter_positive")
+        println("Negative : $counter_negative")
+    */
+
+    //  println(h)
+    /*for (i in 1..n) {
+        if (h[i] % 2 == 0) {
+            counter_Even++
+        }
+        if (h[i] % 2 != 0) {
+            counter_odd++
+        }
+        if ((h[i] > 0)) {
+            counter_positive++
+        }
+        if ((h[i] < 0)) {
+            counter_negative++
+        }
+    }
+    println("Even : $counter_Even")
+    println("Odd : $counter_odd")
+    println("Positive : $counter_positive")
+    println("Negative : $counter_negative")*/
+    /* for (i in 1..n) {
+         print(h[i])
+     }*/
 }
-    /* var readinput = readLine()!!.trim()
-    var (aa, bb, cc, dd) = readinput.split(" ")
-    var a = aa.toDouble()
-    var b = bb.toDouble()
-    var c = cc.toDouble()
-    var d = dd.toDouble() */
-
 
