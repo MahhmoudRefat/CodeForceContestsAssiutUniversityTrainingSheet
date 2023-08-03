9ffe738ef69e21276a01444c2273f5ff838cd935
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-03 18:41:19 +0300

    I - Palindrome  problem  in class code force

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -475,4 +475,20 @@ B. Even Numbers
     if (c == 1) println("YES")
     else println("NO")
      */
+    /*
+    I - Palindrome
+      val sc = Scanner(System.`in`)
+    val a = sc.next()
+    var b = ""
+    for (i in a.length - 1 downTo 0) {
+        b += a[i]
+    }
+    val a1: Int
+    val b2: Int
+    a1 = a.toInt()
+    b2 = b.toInt()
+    println(b2)
+    if (a1 == b2) println("YES") else println("NO")
+
+     */
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,8 @@ import java.util.*
 
 fun main() {
    // var n = readln().toInt()
-    val sc = Scanner(System.`in`)
-    val a = sc.next()
-    var b = ""
-    for (i in a.length - 1 downTo 0) {
-        b += a[i]
-    }
-    val a1: Int
-    val b2: Int
-    a1 = a.toInt()
-    b2 = b.toInt()
-    println(b2)
-    if (a1 == b2) println("YES") else println("NO")
+
+
 
 }
 
