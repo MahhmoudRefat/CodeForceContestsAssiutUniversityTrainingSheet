0e8fe5e7c1e6928e7436ef105611aa9a8639abfe
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-07 09:21:23 +0300

    J. Primes from 1 to n problem  in class code force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,20 @@
 import java.util.*
 
 fun main() {
-   // var n = readln().toInt()
+    // var n = readln().toInt()
 
-
-
+    var n = readln().toInt()
+    for (i in 2..n) {
+        if (IsPrime(i)) {
+            print("$i ")
+        }
+    }
 }
 
+fun IsPrime(a: Int): Boolean {
+    if (a == 2) return true
+     for (i in 2 .. a/2){
+         if (a % i == 0 )return false
+     }
+    return true
+}
