4f9017cf3a40330c48f6987539702f949ec3e331
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-06-21 22:04:27 +0300

    new organize the contests code 11

---
+++ src/main/kotlin/h_Two numbers.kt
@@ -0,0 +1,22 @@
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.roundToInt
+class `h_Two numbers` {
+    fun main (){
+
+
+        var inputline = readLine()!!.trim()
+        var (xx, yy) = inputline.split(" ", limit = 2)
+        var x = xx.toFloat()
+        var y = yy.toFloat()
+        val floorResult = floor(x/y)
+        val ceilResult = ceil(x/y)
+        val roundResult = (x/y).roundToInt()
+        println("floor ${x.toInt()} / ${y.toInt()} = ${floorResult.toInt()}")
+        println("ceil ${x.toInt()} / ${y.toInt()} = ${ceilResult.toInt()}")
+        println("round ${x.toInt()} / ${y.toInt()} = ${roundResult.toInt()}")
+
+        //var floor : Int = x / y
+        //  var ceil : Int = floor + 1
+    }
+}
