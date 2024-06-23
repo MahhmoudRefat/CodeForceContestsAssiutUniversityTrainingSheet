f29f63dc35b8dc74a860d90787a0f9ae7c18a8ae
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-06-23 15:15:45 +0300

    new organize the contests code 13

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -96,20 +96,7 @@ class AcceptedContests {
 
 
 
-    /*
-    J. Multiples
 
-    var inputreader = readln().trim()
-    var (aa,bb) = inputreader.split(" ")
-    var a = aa.toInt()
-    var b = bb.toInt()
-    if (a%b == 0 || b%a == 0 ){
-        println("Multiples")
-    }
-    else {
-        println("No Multiples")
-    }
-     */
     /*
     K. Max and Min
 
---
+++ src/main/kotlin/J_Multiples.kt
@@ -0,0 +1,16 @@
+class J_Multiples{
+
+fun main() {
+
+    var inputreader = readln().trim()
+    var (aa,bb) = inputreader.split(" ")
+    var a = aa.toInt()
+    var b = bb.toInt()
+    if (a%b == 0 || b%a == 0 ){
+        println("Multiples")
+    }
+    else {
+        println("No Multiples")
+    }
+}
+}
