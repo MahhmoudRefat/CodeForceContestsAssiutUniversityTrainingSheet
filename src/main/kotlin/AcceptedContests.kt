5827ad7e582e242f9a323cc9068c2ad4cb52bd25
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-06-25 23:40:20 +0300

    new organize the contests code 14

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -95,19 +95,7 @@ class AcceptedContests {
      */
 
 
-    /*
-    L. The Brothers Problem code force
 
-       var inputline = readln().trim()
-    var (f1, s1) = inputline.split(" ")
-    var inputline2 = readln().trim()
-    var (f2, s2) = inputline2.split(" ")
-    if (s1 == s2) {
-        println("ARE Brothers")
-    } else {
-        println("NOT")
-    }
-     */
 
 /*
 N - Char
---
+++ src/main/kotlin/L_The_Brothers_Problem_code_force.kt
@@ -0,0 +1,13 @@
+class L_The_Brothers_Problem_code_force {
+    fun main (){
+        var inputline = readln().trim()
+        var (f1, s1) = inputline.split(" ")
+        var inputline2 = readln().trim()
+        var (f2, s2) = inputline2.split(" ")
+        if (s1 == s2) {
+            println("ARE Brothers")
+        } else {
+            println("NOT")
+        }
+    }
+}
