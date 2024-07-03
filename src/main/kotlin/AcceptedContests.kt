1431fb551db93888d6056b87da3242ec10a15c52
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-07-04 01:52:29 +0300

    new organize the contests code 18

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -83,19 +83,6 @@ class AcceptedContests {
 
      */
 
-    /*
-    G - Summation from 1 to N
-
-    var x = readLine()!!.toLong()
- //////  var sum : Long= 0
-    for(i in 1..x ){
-       sum += i
-    }/////////
-    println((x*(x+1))/2)
-     */
-
-
-
 
 
 
