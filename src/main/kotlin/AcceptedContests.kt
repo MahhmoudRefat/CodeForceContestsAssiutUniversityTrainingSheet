60e60cfee699570e64cac8c8b2a68636e8f21756
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-07-05 23:12:08 +0300

    new organize the contests code 20

--- src/main/kotlin/AcceptedContests.kt
+++ src/main/kotlin/AcceptedContests.kt
@@ -67,25 +67,4 @@ class AcceptedContests {
     * */
 
 
-    /*F - Digits Summation
-  var inputeline = readLine()!!.trim()
-    var (nn, mm) = inputeline.split(" ", limit = 2)
-    var n = nn.toLong()
-    var m = mm.toLong()
-    println((n % 10 )+( m % 10))
-
-     */
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
---
+++ src/main/kotlin/F_Digits-Summation.kt
@@ -0,0 +1,9 @@
+class `F_Digits-Summation` {
+    fun main (){
+        var inputeline = readLine()!!.trim()
+        var (nn, mm) = inputeline.split(" ", limit = 2)
+        var n = nn.toLong()
+        var m = mm.toLong()
+        println((n % 10 )+( m % 10))
+    }
+}
